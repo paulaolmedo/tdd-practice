6559b43188ccd74dd06251c3e65a5204e9fe350c
paulaolmedo <paulabeatrizolmedo@gmail.com>
2021-08-28 19:51:10 -0300

    refactor

--- roman/roman.go
+++ roman/roman.go
@@ -3,21 +3,31 @@ package roman
 import "errors"
 
 func ConvertToRomanString(arabic int) (string, error) {
+	var romanNumber string
+
 	if arabic <= 0 {
 		return "", errors.New("cannot be negative")
+	} else if arabic >= 10 && arabic < 20 {
+		romanNumber += "X"
+		arabic = arabic - 10
+	} else if arabic >= 20 {
+		romanNumber += "XX"
+		arabic = arabic - 20
 	}
-	var romanNumber string
+
 	if arabic <= 3 {
 		for i := 0; i < arabic; i++ {
 			romanNumber += "I"
 		}
 	} else if arabic == 4 {
-		romanNumber = "IV"
-	} else {
+		romanNumber += "IV"
+	} else if arabic <= 8 {
 		romanNumber += "V"
 		for i := 5; i < arabic; i++ {
 			romanNumber += "I"
 		}
+	} else {
+		romanNumber += "IX"
 	}
 
 	return romanNumber, nil
--- roman/roman_test.go
+++ roman/roman_test.go
@@ -8,16 +8,16 @@ import (
 
 func TestValidNumber(t *testing.T) {
 	arabic1 := 1
-	arabic8 := 8
-
 	roman1, err1 := ConvertToRomanString(arabic1)
-	roman8, err8 := ConvertToRomanString(arabic8)
+
+	arabic9 := 9
+	roman9, err9 := ConvertToRomanString(arabic9)
 
 	assert.Empty(t, err1)
 	assert.Equal(t, "I", roman1)
 
-	assert.Empty(t, err8)
-	assert.Equal(t, "VIII", roman8)
+	assert.Empty(t, err9)
+	assert.Equal(t, "IX", roman9)
 }
 
 func TestInvalidNumber(t *testing.T) {
@@ -30,4 +30,32 @@ func TestInvalidNumber(t *testing.T) {
 
 	assert.Equal(t, expectedRoman, roman)
 	assert.EqualError(t, err, expectedError)
+}
+
+func TestMulti2(t *testing.T) {
+	arabic10 := 10
+	roman10, err10 := ConvertToRomanString(arabic10)
+
+	arabic19 := 19
+	roman19, err19 := ConvertToRomanString(arabic19)
+
+	assert.Empty(t, err10)
+	assert.Equal(t, "X", roman10)
+
+	assert.Empty(t, err19)
+	assert.Equal(t, "XIX", roman19)
+}
+
+func TestMulti3(t *testing.T) {
+	arabic20 := 20
+	roman20, err20 := ConvertToRomanString(arabic20)
+
+	arabic29 := 29
+	roman29, err29 := ConvertToRomanString(arabic29)
+
+	assert.Empty(t, err20)
+	assert.Equal(t, "XX", roman20)
+
+	assert.Empty(t, err29)
+	assert.Equal(t, "XXIX", roman29)
 }
