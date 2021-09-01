7fec29e19ce8b90b1d1b620fa3edd5997d420322
Jesica Grassi <ext_jesgrass@mercadolibre.com>
2021-09-01 11:57:19 -0300

    refactors

--- book-editorial/rest_api/cart_test.go
+++ book-editorial/rest_api/cart_test.go
@@ -6,60 +6,30 @@ import (
 )
 
 func TestEmptyCart(t *testing.T) {
-	cart := Cart{}
+	cart := NewCart()
 	assert.Empty(t, cart.Books)
 }
 
 func TestNotEmptyCart(t *testing.T){
-	editorial := NewEditorial()
-
-	book1 := Book{
-		Isbn:  "isbn1",
-		Price: 120,
-	}
-	book2 := Book{
-		Isbn:  "isbn2",
-		Price: 120,
-	}
-
-	cart := Cart{
-		Id:          "1",
-		Books:       []Book{},
-		TotalAmount: 0,
-		Editorial: editorial,
-	}
-
-	cart.AddBook(book1)
-	cart.AddBook(book2)
+	cart := NewCart()
+	cart.AddBook("isbn1")
+	cart.AddBook("isbn2")
 	assert.NotEmpty(t, cart.Books)
 	assert.Equal(t, 2, len(cart.Books))
 }
 
 func TestShouldNotAddExternalBooks(t *testing.T){
-	cart := Cart{
-		Id:          "1",
-		Books:       []Book{},
-		TotalAmount: 0,
-	}
-	book := Book{
-		Isbn:  "isbn3",
-		Price: 120,
-	}
-	result := cart.AddBook(book)
+	cart := NewCart()
+	result := cart.AddBook("isbn0")
 	assert.False(t, result)
 	assert.Empty(t, cart.Books)
-
-}
-//para la interfaz
-func TestCreateInvalidCart(t *testing.T) {
-	cart, err := NewCart("", "pass")
-	assert.Empty(t, cart)
-	assert.Error(t, err)
-	assert.EqualError(t, err, "1 | Client or Password can not be empty")
 }
 
-func TestCreateCart(t *testing.T) {
-	cart, err := NewCart("mario", "pass")
-	assert.NoError(t, err)
-	assert.NotEmpty(t, cart.Id)
+func TestAddSameBookShouldIncrementQuantity(t *testing.T)  {
+	cart := NewCart()
+	cart.AddBook("isbn1")
+	cart.AddBook("isbn1")
+	assert.NotEmpty(t, cart.Books)
+	assert.Equal(t, 2, cart.Books["isbn1"], "Books quantity must be 2 ")
+
 }
