215149eab8ad549f065581effaf39e4d6a8ac286
Jesica Grassi <ext_jesgrass@mercadolibre.com>
2021-09-01 10:30:55 -0300

    editorial

--- book-editorial/rest_api/cart.go
+++ book-editorial/rest_api/cart.go
@@ -1,5 +1,10 @@
 package rest_api
 
+import (
+	"errors"
+	"github.com/google/uuid"
+)
+
 type Book struct {
 	Isbn string
 	Price float32
@@ -9,10 +14,35 @@ type Cart struct {
 	Id string
 	Books []Book
 	TotalAmount float32
+	Editorial Editorial
 }
 
-func (c Cart) AddBook(book Book) {
-	c.Books = append(c.Books, book)
+func NewCart(client, password string) (Cart, error) {
+	if client == "" || password == ""{
+		return Cart{}, errors.New("1 | Client or Password can not be empty")
+	}
+
+	id, err := uuid.NewUUID()
+	if err != nil{
+		return Cart{}, errors.New("1 | Can not generate id")
+	}
+
+	return Cart{
+		Id:          id.String(),
+		Books:       nil,
+		TotalAmount: 0,
+		Editorial:   Editorial{},
+	}, nil
+}
+
+func (c *Cart) AddBook(book Book) bool{
+	for _, isbn := range c.Editorial.Library {
+		if isbn == book.Isbn {
+			c.Books = append(c.Books, book)
+			return true
+		}
+	}
+	return false
 }
 
 
--- book-editorial/rest_api/cart_test.go
+++ book-editorial/rest_api/cart_test.go
@@ -5,25 +5,41 @@ import (
 	"testing"
 )
 
+func TestCreateInvalidCart(t *testing.T) {
+	cart, err := NewCart("", "pass")
+	assert.Empty(t, cart)
+	assert.Error(t, err)
+	assert.EqualError(t, err, "1 | Client or Password can not be empty")
+}
+
+func TestCreateCart(t *testing.T) {
+	cart, err := NewCart("mario", "pass")
+	assert.NoError(t, err)
+	assert.NotEmpty(t, cart.Id)
+}
+
 func TestEmptyCart(t *testing.T) {
 	cart := Cart{}
 	assert.Empty(t, cart.Books)
 }
 
 func TestNotEmptyCart(t *testing.T){
+	book1 := Book{
+		Isbn:  "isbn1",
+		Price: 120,
+	}
+	book2 := Book{
+		Isbn:  "isbn2",
+		Price: 120,
+	}
+
 	cart := Cart{
 		Id:          "1",
 		Books:       []Book{},
 		TotalAmount: 0,
+		Editorial: Editorial{Library: []string{book1.Isbn, book2.Isbn} },
 	}
-	book1 := Book{
-		Isbn:  "1234567890876",
-		Price: 120,
-	}
-	book2 := Book{
-		Isbn:  "1234567890876",
-		Price: 120,
-	}
+
 	cart.AddBook(book1)
 	cart.AddBook(book2)
 	assert.NotEmpty(t, cart.Books)
@@ -31,5 +47,19 @@ func TestNotEmptyCart(t *testing.T){
 }
 
 func TestShouldNotAddExternalBooks(t *testing.T){
+	editorial := Editorial{Library: []string{"isbn1", "isbn2"}}
+	cart := Cart{
+		Id:          "1",
+		Books:       []Book{},
+		TotalAmount: 0,
+	}
+	book := Book{
+		Isbn:  "isbn3",
+		Price: 120,
+	}
+	result := cart.AddBook(book)
+	assert.False(t, result)
+	assert.Empty(t, cart.Books)
+	assert.NotEmpty(t, editorial.Library)
 
 }
